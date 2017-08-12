aefac6473915e603e02fe6f79f4045f6cc526d31
Nicolas <ntoussai29@gmail.com>
2017-08-12 09:46:12 +0200

    Remove desc when done

--- modules/etudiant/templates/Template_Home.php
+++ modules/etudiant/templates/Template_Home.php
@@ -18,7 +18,7 @@
               <div class="md-list-item-text" layout="column">
                 <h4>#{{etude.numero}} : {{etude.pub_titre}}</h4>
                 <p ng-if="(etude.statut != '2')">Les candidatures sont terminées.</p>
-                <p style="white-space: pre-line;">{{etude.pub | cut}}</p>
+                <p style="white-space: pre-line;" ng-if="(etude.statut == '2')">{{etude.pub | cut}}</p>
                 <p ng-if="(etude.w.statut == '1')">Vous allez recevoir un mail avec la réponse de la JE.</p>
                 <p ng-if="(etude.w.statut == '2')">Vous avez été accepté pour effectuer cette étude. Félicitations !</p>
                 <md-button class="md-secondary" ng-click="postuler($event, etude)" ng-if="!(etude.w.statut > '0') && (etude.statut == 2)">{{ !etude.w  ? 'Candidater' : 'Modifier la candidature'}}</md-button>
