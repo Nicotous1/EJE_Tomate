cca04423fa0eeb822ec26de3c397ba614a8cd583
Nicolas <ntoussai29@gmail.com>
2017-09-20 14:25:24 +0200

    Fixed uppercase in months

--- plugins/WordTemplate/TagShow.php
+++ plugins/WordTemplate/TagShow.php
@@ -137,8 +137,8 @@ function amount2str($a) {
 function date_french($str) {
         $english_days = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
         $french_days = array('Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche');
-        $english_months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'Décember');
-        $french_months = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+        $english_months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+        $french_months = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre');
         return str_replace($english_months, $french_months, str_replace($english_days, $french_days, $str));
 }
 ?>
