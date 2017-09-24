c7009c01037aae39b1c1496efa5fd5d7c5a5c461
Nicolas <ntoussai29@gmail.com>
2017-09-24 23:39:27 +0200

    No text limit for com

--- modules/admin/templates/Template_Etude_Edit.php
+++ modules/admin/templates/Template_Etude_Edit.php
@@ -444,7 +444,7 @@
                 <form ng-submit="save()" layout="column" class="md-padding" ng-if="!etude.locked">
                   <md-input-container flex>
                     <label>Nouveau commentaire</label>
-                    <textarea ng-model="com.content"  rows="5"  md-maxlength="600"></textarea>
+                    <textarea ng-model="com.content"  rows="5"></textarea>
                   </md-input-container>
                   <div layout="row" layout-align="center">
                     <md-button class="md-accent md-raised" ng-if="!etude.locked" ng-disabled="sending" type="submit">{{sending ? 'Sauvegarde en cours...' : 'Poster'}}</md-button>   
