a3a0d0d7300313e4039f084178f72da1a924d781
Nicolas <ntoussai29@gmail.com>
2017-10-02 22:47:14 +0200

    Bold numero

--- modules/admin/templates/Template_Infos.php
+++ modules/admin/templates/Template_Infos.php
@@ -15,7 +15,7 @@
             <md-virtual-repeat-container style="height: 70%;" md-auto-shrink> 
               <md-list-item class="md-3-line" md-virtual-repeat="info in dynamicItems" md-on-demand ng-click="redirect_com(info)" style="border-bottom: solid 1px rgb(220,220,220);">
                 <div class="md-list-item-text" layout="column">
-                  <p style="color: black;">#{{info.etude.numero}} : {{info.author.prenom}} {{info.author.nom}} {{info.type.str_action}}.</p>
+                  <p style="color: black;"><span style="font-weight: bold;">#{{info.etude.numero}}</span> : {{info.author.prenom}} {{info.author.nom}} {{info.type.str_action}}.</p>
                   <p style="padding-left: 15px; white-space: pre-line;" ng-if="info.details">{{info.details}}</p>
                   <p style="text-align: right;">{{info.date | date:'à HH:mm le dd/MM/yyyy'}}</p>
                   <div class="md-secondary" style="padding-left: 15px;"><md-icon>{{info.type.icon}}</md-icon></div>
