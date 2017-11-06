7208ece4c236284fac70b012a2fe27c4a6580021
Nicolas <ntoussai29@gmail.com>
2017-11-06 17:50:44 +0100

    migrate

--- migrate_db/06_11_17.sql
+++ migrate_db/06_11_17.sql
@@ -2,4 +2,4 @@
 ALTER TABLE `entreprise` CHANGE `type` `type` VARCHAR(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL;
 ALTER TABLE `entreprise` CHANGE `secteur` `secteur` VARCHAR(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL;
 UPDATE `entreprise` SET `type`=NULL,`secteur`=NULL;
-ALTER TABLE `entreprise` CHANGE `type` `type` TINYINT(50) NOT NULL, CHANGE `secteur` `secteur` TINYINT(50) NOT NULL;
+ALTER TABLE `entreprise` CHANGE `type` `type` TINYINT(50) NULL, CHANGE `secteur` `secteur` TINYINT(50) NULL;
